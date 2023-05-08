a48e51fa2bd103e6d10b5fa8e9ba41242e5546bb
Mateo Lohezic <mateolohezic@gmail.com>
2023-05-08 15:55:56 -0300

    deploy

--- serpa-administracion/src/Pages/CrearEdificio/CrearEdificio.jsx
+++ serpa-administracion/src/Pages/CrearEdificio/CrearEdificio.jsx
@@ -4,7 +4,7 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { createRouter } from '@remix-run/router';
 import Cookies from 'js-cookie'
-import './CrearEdificio.css'
+import './crearEdificio.css'
 
 const CrearEdificio = () => {
 
