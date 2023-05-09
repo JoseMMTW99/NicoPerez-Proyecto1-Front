c0f3b7e8ce1b207e28e6f161c84c4aadab16c065
JoseMMTW99 <josemmtw99@gmail.com>
2023-05-09 16:25:55 -0300

    footer link

--- serpa-administracion/src/Components/Footer/Footer.jsx
+++ serpa-administracion/src/Components/Footer/Footer.jsx
@@ -5,7 +5,9 @@ function Footer() {
   return (
     <>
         <div className="grupo-2">
+          <a href="https://www.uppering.com.ar/" target='_blank' className='text-decoration-none'>
             <span className='text-muted'>&copy; Desarrollado por Uppering</span>
+          </a>
         </div>
     </>
   )
