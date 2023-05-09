6757c2e9d85939698b7b77bfaa8d110419775f57
Mateo Lohezic <mateolohezic@gmail.com>
2023-05-09 13:46:28 -0300

    error

--- serpa-administracion/src/Pages/Inicio/Inicio.jsx
+++ serpa-administracion/src/Pages/Inicio/Inicio.jsx
@@ -1,18 +1,8 @@
 import React from 'react'
-import Cookies from 'js-cookie'
 import FormularioLogin from '../../Components/FormularioLogin/FormularioLogin'
 import './inicio.css'
 
 function Inicio() {
-
-  const tokenAdmin = Cookies.get('token');
-  const idUser = Cookies.get('id');
-  if (tokenAdmin !== undefined) {
-    window.location.replace('/Administracion')
-  } else if (idUser !== undefined) {
-    window.location.replace('/Perfil')
-  }
-
 
   return (
     <>
