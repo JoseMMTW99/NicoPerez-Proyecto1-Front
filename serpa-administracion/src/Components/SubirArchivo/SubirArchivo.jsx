46cd4e1ed43832f675b1880aa414ebb1ba556bed
Mateo Lohezic <mateolohezic@gmail.com>
2023-06-09 15:36:39 -0300

    update etapa 2

--- serpa-administracion/src/Components/SubirArchivo/SubirArchivo.jsx
+++ serpa-administracion/src/Components/SubirArchivo/SubirArchivo.jsx
@@ -47,7 +47,7 @@ function SubirArchivo(usuario) {
   return (
     <>
       <form onSubmit={handleSubmit(onSubmit)} className="formSubirArchivo">
-        <div className="input-group w-75 mx-auto">
+        <div className="input-group w-100 mx-auto">
           <input
             type="file"
             className="form-control"
